ce09bc4f7984c9b68df98ce680cc803245dd3cb4
Rajat Kumar Dua <gpt75733@gmail.com>
2026-04-27 14:05:52 +0530

    Update AuthNavigator.tsx

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,8 +1,10 @@
+// src/navigation/AuthNavigator.tsx
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { SplashScreen } from '../screens/auth/SplashScreen'
 import { OnboardingScreen } from '../screens/auth/OnboardingScreen'
 import { SignInScreen } from '../screens/auth/SignInScreen'
+import { EmailVerifyScreen } from '../screens/auth/EmailVerifyScreen'
 import { PhoneOTPScreen } from '../screens/auth/PhoneOTPScreen'
 import { ForgotPasswordScreen } from '../screens/auth/ForgotPasswordScreen'
 
@@ -10,6 +12,7 @@ export type AuthStackParams = {
   Splash: undefined
   Onboarding: undefined
   SignIn: undefined
+  EmailVerify: { email: string }
   PhoneOTP: { phone: string }
   ForgotPassword: undefined
 }
@@ -18,16 +21,11 @@ const Stack = createNativeStackNavigator<AuthStackParams>()
 
 export function AuthNavigator() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-        animation: 'fade',
-        contentStyle: { backgroundColor: '#05050F' },
-      }}
-    >
+    <Stack.Navigator screenOptions={{ headerShown: false, animation: 'fade' }}>
       <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="Onboarding" component={OnboardingScreen} />
       <Stack.Screen name="SignIn" component={SignInScreen} />
+      <Stack.Screen name="EmailVerify" component={EmailVerifyScreen} />
       <Stack.Screen name="PhoneOTP" component={PhoneOTPScreen} />
       <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
     </Stack.Navigator>
